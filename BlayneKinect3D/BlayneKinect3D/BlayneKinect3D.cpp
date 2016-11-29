945b3de737ea019ada47c86fb086381c4901076f
Blayne Bradley <blayne.bradley@gmail.com>
2016-11-29 07:25:21 -0500

    Dependency folders.

---
+++ BlayneKinect3D/BlayneKinect3D/BlayneKinect3D.cpp
@@ -0,0 +1,38 @@
+class BlayneKinect3D {
+private:
+
+public:
+	BlayneKinect3D() { 
+	
+	}
+
+	~BlayneKinect3D()
+	{
+
+	}
+
+	bool Init()
+	{
+		return true;
+	}
+
+	void RenderScene()
+	{
+
+	}
+
+	void KeyboardHandler()
+	{
+
+	}
+
+	void MouseClickHandler()
+	{
+
+	}
+
+	void PassiveMouseHandler()
+	{
+
+	}
+};
